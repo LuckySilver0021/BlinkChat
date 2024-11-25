5c45842566fe97599778fd4885ff6a31e8350b00
Sharan Irani <iranisharan@gmail.com>
2024-11-25 21:52:39 +0530

    edited vitejs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'frontend/dist', 
   },
   server: {
     port: 3000,
@@ -15,5 +15,6 @@ export default defineConfig({
     },
   },
 });
+
 
 
